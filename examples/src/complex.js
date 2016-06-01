a568780eb75b9c138c50b87d9a90dec477548e25
Алексей <a@guava.by>
2016-06-02 01:12:31 +0300

    init

--- examples/src/complex.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import OrthodoxCalendar from '../../lib/index';
-
-ReactDOM.render(
-	<OrthodoxCalendar dayicon={true} trapeza_img={true} />,
-	document.getElementById('root')
-);
--- examples/src/simple.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import OrthodoxCalendar from '../../lib/index';
-
-ReactDOM.render(
-	<OrthodoxCalendar />,
-	document.getElementById('root')
-);
